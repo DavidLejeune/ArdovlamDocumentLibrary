bb427190edcc620abfe109e595d38effff48b6c9
David Lejeune <David Lejeune>
2017-03-08 11:24:07 +0100

    delete files no longer in csv's (doc, com & tec)

--- app/src/main/java/com/lejeune/david/ardovlamdocumentlibrary/AsyncRemoveOldFilesDL.java
+++ app/src/main/java/com/lejeune/david/ardovlamdocumentlibrary/AsyncRemoveOldFilesDL.java
@@ -6,7 +6,10 @@ import android.util.Log;
 
 import org.apache.commons.net.ftp.FTPFile;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -17,18 +20,25 @@ import java.util.ArrayList;
 
 public class AsyncRemoveOldFilesDL extends AsyncTask{
 
-    private static ArrayList<String> listFilesDoc;
-    private static ArrayList<String> listFilesTec;
-    private static ArrayList<String> listFilesCom;
+    private static ArrayList<String> listFilesDocLocal;
+    private static ArrayList<String> listFilesTecLocal;
+    private static ArrayList<String> listFilesComLocal;
 
+    private static ArrayList<String> listFilesDocCSV;
+    private static ArrayList<String> listFilesTecCSV;
+    private static ArrayList<String> listFilesComCSV;
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
 
-        listFilesDoc = new ArrayList<>();
-        listFilesCom = new ArrayList<>();
-        listFilesTec = new ArrayList<>();
+        listFilesDocLocal = new ArrayList<>();
+        listFilesComLocal = new ArrayList<>();
+        listFilesTecLocal = new ArrayList<>();
+
+        listFilesDocCSV = new ArrayList<>();
+        listFilesComCSV = new ArrayList<>();
+        listFilesTecCSV = new ArrayList<>();
 
 
     }
@@ -36,10 +46,20 @@ public class AsyncRemoveOldFilesDL extends AsyncTask{
     @Override
     protected void onPostExecute(Object o) {
         super.onPostExecute(o);
-        showArrayListDoc();
-        showArrayListCom();
-        showArrayListTec();
+        showArrayListDocLocal();
+        showArrayListComLocal();
+        showArrayListTecLocal();
+        showArrayListDocCSV();
+        showArrayListComCSV();
+        showArrayListTecCSV();
 
+        compareFileList(listFilesDocLocal , listFilesDocCSV , "Documents");
+        compareFileList(listFilesComLocal , listFilesComCSV , "Commercial");
+        compareFileList(listFilesTecLocal , listFilesTecCSV , "Technical");
+
+//        loopCSVfile("Documents.csv" , listFilesDocLocal);
+//        loopCSVfile("Commercial.csv" , listFilesComLocal);
+//        loopCSVfile("Technical.csv" , listFilesTecLocal);
 
 
 
@@ -49,14 +69,18 @@ public class AsyncRemoveOldFilesDL extends AsyncTask{
     @Override
     protected Object doInBackground(Object[] objects) {
 
-        createListOfFiles(MyVars.FOLDER_DOCUMENTS , listFilesDoc);
-        createListOfFiles(MyVars.FOLDER_COMMERCIAL , listFilesCom);
-        createListOfFiles(MyVars.FOLDER_TECHNICAL , listFilesTec);
+        createListOfFilesLocal(MyVars.FOLDER_DOCUMENTS , listFilesDocLocal);
+        createListOfFilesLocal(MyVars.FOLDER_COMMERCIAL , listFilesComLocal);
+        createListOfFilesLocal(MyVars.FOLDER_TECHNICAL , listFilesTecLocal);
+
+        createListOfFilesCSV("Documents.csv" , listFilesDocCSV);
+        createListOfFilesCSV("Commercial.csv" , listFilesComCSV);
+        createListOfFilesCSV("Technical.csv" , listFilesTecCSV);
         return null;
     }
 
 
-    private void createListOfFiles(String strFolder , ArrayList listFiles){
+    private void createListOfFilesLocal(String strFolder , ArrayList listFiles){
 
         String path = Environment.getExternalStorageDirectory().toString()+ strFolder;
         File directory = new File(path);
@@ -79,34 +103,142 @@ public class AsyncRemoveOldFilesDL extends AsyncTask{
 
 
     }
+    private void createListOfFilesCSV(String fileName , ArrayList listFiles){
+        File dir = Environment.getExternalStorageDirectory();
+        File file = new File(dir, MyVars.FOLDER_DATA + fileName);
+
+        MyTimer myTimer = new MyTimer("loopCSVfile " + fileName);
+        String strFile = "";
+        String line = "";
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                while ((line = br.readLine()) != null) {
+                    if (line.length() > 0) {
+                        {
+                            String[] str = line.split(",");
+                            //System.out.println(iCount + " " + line);
+
+                            strFile = str[2].toLowerCase();
+                            listFiles.add(strFile);
+                        }
+
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            myTimer.getElapsedTime();
 
 
-    private void showArrayListDoc(){
+        }
+        else
+        {
+            System.out.println("File documents doc not found");
+        }
+
+    }
+
+
+    private void showArrayListDocCSV(){
         System.out.println("looping through the arraylist");
-        for (String cu : listFilesDoc) {
+        for (String cu : listFilesDocCSV) {
+            System.out.println("list DOC files csv: " + cu);
+        }
+    }
+    private void showArrayListComCSV(){
+        System.out.println("looping through the arraylist");
+        for (String cu : listFilesComCSV) {
+            System.out.println("list COM files csv: " + cu);
+        }
+    }
+    private void showArrayListTecCSV(){
+        System.out.println("looping through the arraylist");
+        for (String cu : listFilesTecCSV) {
+            System.out.println("list TEC files csv: " + cu);
+        }
+    }
+
+    private void showArrayListDocLocal(){
+        System.out.println("looping through the arraylist");
+        for (String cu : listFilesDocLocal) {
             System.out.println("list DOC files : " + cu);
         }
     }
-    private void showArrayListCom(){
+    private void showArrayListComLocal(){
         System.out.println("looping through the arraylist");
-        for (String cu : listFilesCom) {
+        for (String cu : listFilesComLocal) {
             System.out.println("list COM files : " + cu);
         }
     }
-    private void showArrayListTec(){
+    private void showArrayListTecLocal(){
         System.out.println("looping through the arraylist");
-        for (String cu : listFilesTec) {
+        for (String cu : listFilesTecLocal) {
             System.out.println("list TEC files : " + cu);
         }
     }
 
-    private void loopCSVfile(File file){
+    private void compareFileList(ArrayList local , ArrayList csv , String strDepartment){
+        MyTimer myTimer = new MyTimer("compareFileList ");
+        String fileNameLocal = "";
+        String fileNameCSV = "";
+        File dir = Environment.getExternalStorageDirectory();
 
+
+        for (Object localFile : local)
+        {
+            Boolean foundInCSV = false;
+
+            fileNameLocal = localFile.toString().toLowerCase();
+            for (Object csvFile : csv) {
+                fileNameCSV = csvFile.toString().toLowerCase();
+                if(fileNameCSV.equalsIgnoreCase(fileNameLocal)){
+                    foundInCSV = true;
+                }
+            }
+
+
+            if(!foundInCSV)
+            {
+                System.out.println("This file needs to be deleted : " + fileNameLocal);
+
+
+                File fileToDelete = null;
+                File imgToDelete = null;
+                String strImg = fileNameLocal.replace(".pdf", ".png");
+                if (strDepartment.equalsIgnoreCase("Documents")){
+                    fileToDelete = new File(dir, MyVars.FOLDER_DOCUMENTS + fileNameLocal);
+                    imgToDelete = new File(dir, MyVars.FOLDER_DOCUMENTS + strImg);
+                }
+                if (strDepartment.equalsIgnoreCase("Commercial")){
+                    fileToDelete = new File(dir, MyVars.FOLDER_COMMERCIAL + fileNameLocal);
+                    imgToDelete = new File(dir, MyVars.FOLDER_COMMERCIAL + strImg);
+                }
+                if (strDepartment.equalsIgnoreCase("Technical")){
+                    fileToDelete = new File(dir, MyVars.FOLDER_TECHNICAL + fileNameLocal);
+                    imgToDelete = new File(dir, MyVars.FOLDER_TECHNICAL + strImg);
+                }
+                deleteFile(fileToDelete,imgToDelete);
+            }
+
+        }
+        myTimer.getElapsedTime();
 
     }
 
-    private void deleteFile(File file){
-        
+
+    private void deleteFile(File file,File img){
+        boolean deleted = file.delete();
+        System.out.println(file.toString() + " delete status  = " + deleted);
+        if (!file.exists()) {
+            System.out.println("succesfully deleted file");
+        }
+        boolean deletedImg = img.delete();
+        System.out.println(img.toString() + " delete status img = " + deletedImg);
+        if (!img.exists()) {
+            System.out.println("succesfully deleted file");
+        }
     }
 
 }
